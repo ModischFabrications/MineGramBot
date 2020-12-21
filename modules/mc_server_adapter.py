04bd8771c401c5d11c09298f0fff8251f00a8dc0
ModischFabrications <modisch.fabrications@gmail.com>
2020-12-21 21:16:13 +0100

    add comments

--- modules/mc_server_adapter.py
+++ modules/mc_server_adapter.py
@@ -8,5 +8,7 @@ def start_server():
     if not command:
         raise AttributeError("command missing!")
 
+    # this is not nice form, but easier than creating a dedicated startup script
+    # no shell escape! Make sure no garbage is passed!
     process = subprocess.run(config.COMMAND, shell=True, capture_output=True, text=True)
     print(process)
