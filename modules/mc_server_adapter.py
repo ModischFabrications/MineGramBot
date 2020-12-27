907a001d7d03bfde2c2d05eb48e7f164e2a3dad9
Eisdrache <johannes.roeske@web.de>
2020-12-27 19:15:23 +0100

    Added stop_server functionality and added a start.sh script

--- modules/mc_server_adapter.py
+++ modules/mc_server_adapter.py
@@ -4,11 +4,23 @@ import config
 
 
 def start_server():
-    command = config.COMMAND
+    command = config.COMMAND_START
     if not command:
         raise AttributeError("command missing!")
 
     # this is not nice form, but easier than creating a dedicated startup script
     # no shell escape! Make sure no garbage is passed!
-    process = subprocess.run(config.COMMAND, shell=True, capture_output=True, text=True)
+    process = subprocess.run(config.COMMAND_START, shell=True)
     print(process)
+
+
+def stop_server():
+    command = config.COMMAND_STOP
+    if not command:
+        raise AttributeError("command missing!")
+
+    # this is not nice form, but easier than creating a dedicated startup script
+    # no shell escape! Make sure no garbage is passed!
+    process = subprocess.run(config.COMMAND_STOP, shell=True)
+    print(process)
+
